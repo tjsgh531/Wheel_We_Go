df7bbb3ccd70e2a94b722e4b3c59714113900992
ganggangme <kga0972@gmail.com>
2023-08-15 19:03:00 +0900

    지역색변하기 합침

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
     mode:"development",
     entry:{
         base :"./js/base.js",
+        service: "./js/service.js",
     },
     output:{
         path:path.resolve(__dirname, "static", "js"),
