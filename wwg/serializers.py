e427457828702adbed190b56ce8d692a0eafb57a
이선호 <tjsgh5312@daum.net>
2023-08-15 21:30:24 +0900

    두팀 합쳐졌어요

--- wwg/serializers.py
+++ wwg/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Users, Records, Regions, Markings
+from .models import kakaoUsers, Records, Regions, Markings
 
 
 
@@ -8,7 +8,7 @@ from .models import Users, Records, Regions, Markings
 ### 모델마다 모든 레코드의 CRUD Serializer ###
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Users
+        model = kakaoUsers
         fields = '__all__'
 
 class RecordsSerializer(serializers.ModelSerializer):
@@ -33,12 +33,6 @@ class RecordsFilterSerializer(serializers.ModelSerializer):
         model = Records
         fields = ('start_location','end_location','TIME','feedback','credits_earned','record_date','data_valid','km')
 
-
-### 이동거리 값 반환 ###
-class DataSerializer(serializers.Serializer):
-    dong = serializers.CharField(max_length=100)
-    mapCount = serializers.IntegerField()
-    mapDistance = serializers.IntegerField()
 
 
 
