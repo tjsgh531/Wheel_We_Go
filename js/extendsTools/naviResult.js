4c2e2877b3f80a2b78096f92a94b9d38e3cf9078
이선호 <tjsgh5312@daum.net>
2023-08-16 21:48:24 +0900

    result Board 만드는 중

---
+++ js/extendsTools/naviResult.js
@@ -0,0 +1,17 @@
+export class NaviResult{
+    constructor(){
+        
+    }
+
+    // map 위치 설정
+    settingMapPosition(){
+        
+    }
+
+    // 결과창 만드는 것
+    createResultBoard(data_validation, time, distance, coin){
+
+    }
+
+
+}
