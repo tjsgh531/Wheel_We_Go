c226eb8c11c772bbfecb29de9b9cdb95be533986
psuno01 <p20035357@gmail.com>
2023-08-17 17:25:34 +0900

    mydata

--- js/mydata.js
+++ js/mydata.js
@@ -1,9 +1,9 @@
 import { RestApiData } from "./extendsTools/restApiData.js";
 import { NaviResult } from "./extendsTools/naviResult.js";
 
-class MyData{
+class MyData {
   //흐름
-  constructor(){
+  constructor() {
     //restApiData.js에서 함수를 쓰기 위한 추가
     this.restApiTool = new RestApiData();
     //naveResultTool.js에서 함수를 쓰기 위한 추가
@@ -16,30 +16,30 @@ class MyData{
     // this.setArrayTime();
     //현재 로그인 된 유저의 모든 데이터 가져오기
     this.PrintUserRecordsAllData();
-    
+
   }
   ////////////////////// 사용자 누구인지 return 함수 /////////////////////
 
-  getCurrentLoginUser(){
-      //userinfo에 html에 있는 id가져오기
-      let userinfo = document.getElementById('user-info');
-      // user에 data-username에 있는 유저네임 저장  
-      let user = userinfo.dataset.username;
-      // 이후 user 변수를 활용하여 필요한 작업을 수행할 수 있습니다.
-      return user;
+  getCurrentLoginUser() {
+    //userinfo에 html에 있는 id가져오기
+    let userinfo = document.getElementById('user-info');
+    // user에 data-username에 있는 유저네임 저장  
+    let user = userinfo.dataset.username;
+    // 이후 user 변수를 활용하여 필요한 작업을 수행할 수 있습니다.
+    return user;
   }
 
   ///////////////////// 데이터 정렬 기준을 위한 토글 //////////////////////
-  
-  setArrayTime(){
+
+  setArrayTime() {
 
 
 
   }
   /////////////////////// 지역 별 데이터 토글 //////////////////////////////
-  
-  setArrayRegion(){
-    
+
+  setArrayRegion() {
+
   }
 
   ////////////////////// 로그인 된 사용자의 기록 저장 순으로 출력 /////////////////////
@@ -56,18 +56,19 @@ class MyData{
         const end = infoData.info.endName;
         const time = infoData.info.AtTime;
         const km = infoData.info.distance;
-        const credit =infoData.earnedCoin;
+        const credit = infoData.earnedCoin;
         const listItem = document.createElement('li');
         listItem.style.listStyle = 'none';
         listItem.innerHTML = `
             <div class="route-data-time">${recordDate}</div>
-            <div class="circle-container">
+            <div class="route-etc-time">
+              <div class="circle-container">
                 <span class="circle-0"></span>
                 <div class="route">
                     <div class='start_location'>${start}</div> -> <div class ='end_location'>${end}</div>
                 </div>
-            </div>
-            <div class="etc-data">
+              </div>
+              <div class="etc-data">
                 <div><img src="../static/img/시간.png" alt="시간">
                     <div class = 'take_time'>${time}분</div>
                 </div>
@@ -78,12 +79,13 @@ class MyData{
 
                     <div class='coins'>${credit}</div>
                 </div>
+              </div>
             </div>
         `;
         document.querySelector('.records-list').appendChild(listItem);
+      });
     });
-});
-}
+  }
   /////////////////////////////// 출력 ///////////////////////////////
 
 }
@@ -91,6 +93,6 @@ class MyData{
 
 
 
-window.onload = ()=>{
+window.onload = () => {
   new MyData();
 }
