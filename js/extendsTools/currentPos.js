20b1d4373ae5e9004eb4c43035b25e9a2e4d35d2
이선호 <tjsgh5312@daum.net>
2023-08-19 00:22:00 +0900

    가라

--- js/extendsTools/currentPos.js
+++ js/extendsTools/currentPos.js
@@ -20,31 +20,18 @@ export class CurrentPos{
     
 
     async getCurrentLocation() {
-        const location = new Promise((resolve, reject) => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(resolve, reject);
-            } else {
-                reject(new Error('Geolocation이 지원되지 않는 브라우저입니다.'));
+        return new Promise((resolve, reject) => {
+            const init_position = {
+                coords :{    
+                    latitude : 37.468478,
+                    longitude : 127.039257,
+                }                
             }
-        })
-        .catch(error => {
-            console.error('오류:', error.message);
-        });;
+            resolve(init_position);
+        });
 
-        const timeout = new Promise((resolve, reject)=>{
-            setTimeout(()=>{
-                //AT센터로 좌표 이동..
-                this.init_position = {
-                    coords :{    
-                        latitude : 37.468478,
-                        longitude : 127.039257,
-                    }                
-                }
-                console.log("시간이 지났어요...");
-                resolve(this.init_position);
-            }, 15000);
-        })
+  
 
-        return Promise.race([location, timeout]);
+
     }
 }
